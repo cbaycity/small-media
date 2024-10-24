9539d6354b69f636f0cdae774554fb80e0fc7cf5
Charles Carlson <charlescarlson@Bayards-Macbook-Pro.local>
2024-10-24 07:07:22 -0700

    Routing correctly added

--- art-hound/src/client/src/components/Header.tsx
+++ art-hound/src/client/src/components/Header.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import LocalArt from "./LocalArt";
-import PopularArt from "./PopularArt";
-import UserProfile from "./UserProfile";
-import Friends from "./Friends";
+import { Link } from "react-router-dom"
 
 
 function artHoundHeader() {
@@ -15,17 +9,15 @@ function artHoundHeader() {
             <div className="logo">
             <img src="/public/artHoundLogo.png" alt="Art Hound Logo"/>
             </div>
-            <Router>
                 <div className="navigation">
                     <ul className="nav-list">
                         <li> <Link to="/LocalArt" className="navigation-link px-2 link-two">Local Art</Link></li>
                         <li> <Link to="/PopularArt" className="navigation-link px-2 link-two">Popular Art</Link></li>
-                        <li> <Link to="/Profile" className="navigation-link px-2 link-two">My Art</Link></li>
+                        <li> <Link to="/MyArt" className="navigation-link px-2 link-two">My Art</Link></li>
                         <li> <Link to="/Friends" className="navigation-link px-2 link-two">Friends</Link></li>
                         <li> <Link to="/About" className="navigation-link px-2 link-two">About</Link></li>
                     </ul>
                 </div>
-            </Router>
             <div className="login">
                 <div className="login-button">
                 <button type="button" className="login-button">Login</button>
