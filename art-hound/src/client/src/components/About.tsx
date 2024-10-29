386445adb2e77e3cd687b3b9df828b68959e2170
Charles Carlson <charlescarlson@Bayards-Macbook-Pro.local>
2024-10-28 21:23:33 -0700

    About me adjusted slightly

--- art-hound/src/client/src/components/About.tsx
+++ art-hound/src/client/src/components/About.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function About() {
     return(
-    <div className="container">
-        <p>Need to build the About website.</p>
+    <div className="container about-me-body">
+        <div className="p40">
+            <img className="portrait" src="public/BayardPhoto.png"></img>
+        </div>
+        <div className="p40">
+            <p>Art Hound is created by Bayard Carlson, a </p>
+        </div>
     </div>
     );
 };
