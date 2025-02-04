1292d906c74863d3e8d22508cd9bc162e86585e1
Charles Carlson <charlescarlson@Bayards-Macbook-Pro.local>
2025-02-04 11:49:34 -0800

    Gets the styling done

--- art-hound/src/client/src/components/Search.tsx
+++ art-hound/src/client/src/components/Search.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { LeftBar, RightBar } from './Sidebars'
 
 function Search() {
     return (
-        <div className="container center-body general-body-background">
-            <p>Need to build the Search website.</p>
+        <div className="container center-body general-center-body general-body-background">
+            <LeftBar/>
+            <div className="general-center-body">
+            <h2>Search for a friend's username.</h2>
+            </div>
+            <RightBar/>
         </div>
     )
 }
